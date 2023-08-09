ad52e1748ab2f0af93ea6ceccfe16de53eb136da
Coffee58511867 <marobahlalele910@gmail.com>
2023-08-09 14:20:34 +0200

    Created pages

--- src/app/components/Header/page.tsx
+++ src/app/components/Header/page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+'use client'
+
+import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div style={styles.header}>
+      <div style={styles.linksContainer}>
+        <Link href='/pages/register' >About Us</Link>
+        <Link href='/pages/Login' >Login</Link>
+        <Link href='/pages/register'>Register</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+const styles = {
+  header: {
+    backgroundColor: '#333',
+    padding: '10px 0',
+  },
+  linksContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '20px',
+  },
+  link: {
+    color: 'white',
+    textDecoration: 'none',
+    fontSize: '18px',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: '1px',
+    padding: '8px 16px',
+    border: '1px solid white',
+    borderRadius: '4px',
+    transition: 'background-color 0.3s, color 0.3s',
+  },
+};
+
+export default Header;
