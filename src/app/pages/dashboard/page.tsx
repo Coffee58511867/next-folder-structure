6a1fd2420f93e05d010f52d19e3bb0b6b5e67957
Coffee58511867 <marobahlalele910@gmail.com>
2023-08-09 14:10:20 +0200

    Created pages

---
+++ src/app/pages/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/app/components/Header/page'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Dashboard Page</h1>
+    </div>
+  )
+}
+
+export default Dashboard
